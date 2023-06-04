19e523a2bdeec3db6dbe3428ec7d2f33a0d4f30e
Starcea <stardev.uwu@gmail.com>
2023-06-04 16:21:30 +0900

    refactor: val name

--- dibug/utils/chunk.py
+++ dibug/utils/chunk.py
@@ -9,9 +9,9 @@ def chunk_string(value: str, limit: int) -> list[str]:
             chunks.append(value[start:])
             break
 
-        last_space = value.rfind("\n", start, end)
-        if last_space != -1 and last_space > start:
-            end = last_space
+        last_newline = value.rfind("\n", start, end)
+        if last_newline != -1 and last_newline > start:
+            end = last_newline
 
         chunks.append(value[start:end])
         start = end + 1
